18ed720d5d3b8c6c5fd23954996bc42388a2736b
Oleh Stasula <oleh.stasula@gmail.com>
2017-06-07 12:55:55 +0300

    Added bootstrap styles

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
+import { Grid, Row, Col } from 'react-bootstrap'
 import { render } from 'react-dom'
 
 import ChatBot from '../../src'
 
 class Demo extends Component {
   render () {
-    return <div>
-      <h1>Hi</h1>
-      <ChatBot />
-    </div>
+    return (
+      <Grid>
+        <Row>
+          <Col xs={12} xsOffset={0} sm={10} smOffset={1} md={8} mdOffset={2}>
+            <h1>Hi</h1>
+            <ChatBot />
+          </Col>
+        </Row>
+      </Grid>
+    )
   }
 }
 
