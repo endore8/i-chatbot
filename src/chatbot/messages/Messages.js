7aee45ffd94521af55b0138c88aa77fdcdc1c94d
Oleh Stasula <oleh.stasula@gmail.com>
2017-06-16 13:08:44 +0300

    Autoscroll

--- src/chatbot/messages/Messages.js
+++ src/chatbot/messages/Messages.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Scroll from 'react-scroll'
 
 import Message from './message/Message'
 import Typing from './typing/Typing'
 
 import './Messages.css'
 
+const Element = Scroll.Element
+const Scroller = Scroll.scroller
+
 class Messages extends Component {
+  componentDidUpdate (prevProps) {
+    if (this.props.messages.length === prevProps.messages.length + 1 || this.props.isTyping !== prevProps.isTyping) {
+      Scroller.scrollTo('LastMessage', {
+        duration: 500,
+        smooth: true,
+        containerId: 'Messages'
+      })
+    }
+  }
+
   render () {
     return (
-      <ul className='Messages'>
+      <ul className="Messages" id="Messages">
         {this.props.messages.map((message, i) => <Message {...message} key={i} />)}
         {this.props.isTyping && <Typing />}
+        <Element name="LastMessage" />
       </ul>
     )
   }
