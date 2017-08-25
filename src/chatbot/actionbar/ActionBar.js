a5cbbfa57814732a3504c4d38933dfacba75c491
Oleh Stasula <oleh.stasula@gmail.com>
2017-08-25 12:22:03 +0300

    Horizontal items in action bar

--- src/chatbot/actionbar/ActionBar.js
+++ src/chatbot/actionbar/ActionBar.js
@@ -9,18 +9,20 @@ class ActionBar extends Component {
   render () {
     return (
       <div className="ActionBar">
-        {this.props.actions && this.props.actions.map((action, i) => {
-          switch (action.type) {
-            case 'get-started':
-              return <GetStartedButton {...action} key={i} />
+        <ul className="Items">
+          {this.props.actions && this.props.actions.map((action, i) => {
+            switch (action.type) {
+              case 'get-started':
+                return <li><GetStartedButton {...action} key={i} /></li>
 
-            case 'quick-reply':
-              return <ReplyButton {...action} key={i} />
+              case 'quick-reply':
+                return <li><ReplyButton {...action} key={i} /></li>
 
-            case 'text-input':
-              return <TextInput {...action} key={i} />
-          }
-        })}
+              case 'text-input':
+                return <li><TextInput {...action} key={i} /></li>
+            }
+          })}
+        </ul>
       </div>
     )
   }
