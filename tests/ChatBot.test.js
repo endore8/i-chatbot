3113245830469d9c58ed0895e3e14c0a3a4dc68f
Oleh Stasula <oleh.stasula@gmail.com>
2017-08-27 17:46:45 +0300

    Some chatbot tests

--- tests/ChatBot.test.js
+++ tests/ChatBot.test.js
@@ -7,8 +7,6 @@ import ChatBot, { ChatBotUtil } from 'src/'
 describe('ChatBot', () => {
   let node
 
-  const onGetStarted = () => {}
-
   beforeEach(() => {
     node = document.createElement('div')
   })
@@ -18,11 +16,46 @@ describe('ChatBot', () => {
   })
 
   it('Renders view', () => {
+    const onGetStarted = () => {}
+
     render(
       <ChatBot getStartedButton={ChatBotUtil.makeGetStartedButton('Start')}
                onGetStarted={onGetStarted} />, node, () => {
         expect(node.children.length).toEqual(1)
         expect(node.firstChild.className).toEqual('I-ChatBot')
+      }
+    )
+  })
+
+  it('Calls onGetStarted callback on startOver with no start button', () => {
+    let isCalled = false
+    let chat = null
+    const onGetStarted = () => {
+      isCalled = true
+    }
+
+    render(
+      <ChatBot onGetStarted={onGetStarted}
+               ref={(c) => { chat = c }} />, node, () => {
+        chat.startOver()
+        expect(isCalled).toEqual(true)
+      }
+    )
+  })
+
+  it('Does not call onGetStarted callback on startOver with start button', () => {
+    let isCalled = false
+    let chat = null
+    const onGetStarted = () => {
+      isCalled = true
+    }
+
+    render(
+      <ChatBot getStartedButton={ChatBotUtil.makeGetStartedButton('Start')}
+               onGetStarted={onGetStarted}
+               ref={(c) => { chat = c }} />, node, () => {
+        chat.startOver()
+        expect(isCalled).toEqual(false)
       }
     )
   })
